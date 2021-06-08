0202aed63aa7e360557b31b33bfa45248e0b7a2d
20013327 <20013327@myrp.edu.sg>
2021-06-08 21:41:58 +0800

    Done but I edited here and there so it does not really tally with some of the problem statement enhancement

--- app/src/main/java/sg/edu/rp/c346/id20013327/simpletodol07/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20013327/simpletodol07/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20013327.simpletodol07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         btnDelete = findViewById(R.id.btnDelete);
         spnAddRemove = findViewById(R.id.spinnerChoice);
 
+
         alTask = new ArrayList<String>();
 
         aaTask = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, alTask);
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String task = etTask.getText().toString();
+//                int index = Integer.parseInt(etTask.getText().toString());
                 if(task.isEmpty() == false) {
                     alTask.add(task); //add the user input into the ArrayList, alTask.
                     aaTask.notifyDataSetChanged(); //notifies the attached observers that the underlying data has been changed and any view reflecting the data set should refresh itself.
@@ -68,16 +71,20 @@ public class MainActivity extends AppCompatActivity {
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String task = etTask.getText().toString();
+                int index = Integer.parseInt(etTask.getText().toString());
                 if(alTask.isEmpty() == false) {
-//                    alTask;
+                    if(aaTask.getCount() > index) {
+                        alTask.remove(index);
+                        aaTask.notifyDataSetChanged();
+                    } else if(aaTask.getCount() == 0) {
+                        Toast.makeText(MainActivity.this, "No task to be remove", Toast.LENGTH_LONG).show();
+                    } else if(aaTask.getCount() < index){
+                        Toast.makeText(MainActivity.this, "Invalid index", Toast.LENGTH_LONG).show();
+                    }
 
                 } else {
                     Toast.makeText(MainActivity.this, "There are no activity to be deleted", Toast.LENGTH_SHORT).show();
-                    if(alTask.isEmpty() == true) {
-                        btnClear.setEnabled(false);
-                    } else {
-                        btnClear.setEnabled(true);
-                    }
                 }
             }
         });
@@ -87,6 +94,9 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(position == 0 ) {
 
+                    etTask.setHint("Add a new task");
+                    etTask.setInputType(InputType.TYPE_CLASS_TEXT);
+                    etTask.setText("");
                     btnAdd.setEnabled(true);
                     btnDelete.setEnabled(false);
                     if(alTask.isEmpty() == true) {
@@ -96,10 +106,13 @@ public class MainActivity extends AppCompatActivity {
                     }
                 } else if(position == 1) {
 
+                    etTask.setHint("Remove a existing task");
+                    etTask.setInputType(InputType.TYPE_CLASS_NUMBER);
                     btnDelete.setEnabled(true);
                     btnAdd.setEnabled(false);
                     if(alTask.isEmpty() == true) {
                         btnClear.setEnabled(false);
+                        btnDelete.setEnabled(false);
                     } else {
                         btnClear.setEnabled(true);
                     }
